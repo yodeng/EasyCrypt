b6813ce1f37f2be5de4d3393331567efbeb6740c
dengyong <dengyong@hugobiotech.com>
2021-10-19 12:39:08 +0800

    update and fix re typo

--- src/decrypt.py
+++ src/decrypt.py
@@ -3,14 +3,15 @@ import re
 import base64
 import struct
 
-from Crypto.Cipher import AES
 from Crypto import Random
+from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 
 from .utils import add_to_16
 
 
 class DecryptData(object):
+
     @staticmethod
     def decrypt_string(data, key="G+b8u9XWZljDTy7Lbp/eqdKeAhvC46gl"):
         '''
@@ -40,19 +41,19 @@ class DecryptData(object):
                     chunk = unpad(chunk, AES.block_size)
                 yield chunk
 
-    @staticmethod
-    def decrypt_file(in_filename, out_file=None, chunksize=64*1024, key="G+b8u9XWZljDTy7Lbp/eqdKeAhvC46gl"):
+    @classmethod
+    def decrypt_file(cls, in_filename, out_file=None, chunksize=64*1024, key="G+b8u9XWZljDTy7Lbp/eqdKeAhvC46gl"):
         if out_file is None:
             out_file = in_filename + ".enc"
         with open(out_file, "wb") as fo:
-            for line in DecryptData.decrypt_file_iter(in_filename=in_filename, chunksize=chunksize, key=key):
+            for line in cls.decrypt_file_iter(in_filename=in_filename, chunksize=chunksize, key=key):
                 fo.write(line)
 
-    @staticmethod
-    def decrypt_file_readline(in_filename, key="G+b8u9XWZljDTy7Lbp/eqdKeAhvC46gl"):
+    @classmethod
+    def decrypt_file_readline(cls, in_filename, key="G+b8u9XWZljDTy7Lbp/eqdKeAhvC46gl"):
         line = ""
-        for src in DecryptData.decrypt_file_iter(in_filename, chunksize=64*1024, key=key):
-            res = re.split("\n", src)
+        for src in cls.decrypt_file_iter(in_filename, chunksize=64*1024, key=key):
+            res = re.split("\n", src.decode())
             res[0] = line + res[0]
             yield res[0] + "\n"
             if len(res) == 1:
